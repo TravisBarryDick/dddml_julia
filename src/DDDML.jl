e1b55bffd29a6079ee340a9213dcb23a712e8d28
Travis Dick <travis.barry.dick@gmail.com>
2015-10-29 17:46:06 -0400

    cleaned up DDDML.jl

--- src/DDDML.jl
+++ src/DDDML.jl
@@ -1,13 +1,10 @@
 module DDDML
 
 include("filesplit.jl")
-export filesplit
-
 include("subsample.jl")
-export subsample, subsample_parsed
-
 include("libsvm_parse.jl")
-
 include("filesystem_tools.jl")
+
+export filesplit, subsample
 
 end
