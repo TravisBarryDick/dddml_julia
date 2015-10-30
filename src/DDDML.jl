f49beeddc50712d272af0009d8ea6f393db45757
Travis Dick <travis.barry.dick@gmail.com>
2015-10-30 15:45:04 -0400

    Rudimentary clustering and dispatch.

--- src/DDDML.jl
+++ src/DDDML.jl
@@ -4,11 +4,13 @@ include("filesplit.jl")
 include("subsample.jl")
 include("libsvm_parse.jl")
 include("filesystem_tools.jl")
+include("approx_nns.jl")
+include("kmeans.jl")
 include("dispatcher.jl")
 include("learner.jl")
 include("testing.jl")
 
 export filesplit, subsample, cluster, build_nns, find_nn, random_dispatch
-export train_models, random_testing
+export train_models, random_testing, init_dispatchers, dispatch, testing
 
 end
