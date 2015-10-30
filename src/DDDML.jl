54c5d08ea28674e840a8f497316482053852e0aa
Travis Dick <travis.barry.dick@gmail.com>
2015-10-30 01:02:13 -0400

    Added calls to liblinear.

--- src/DDDML.jl
+++ src/DDDML.jl
@@ -7,6 +7,7 @@ include("filesystem_tools.jl")
 #include("kmeans.jl")
 #include("approx_nns.jl")
 include("dispatcher.jl")
+include("learner.jl")
 
 export filesplit, subsample, cluster, build_nns, find_nn, random_dispatch
 
--- src/dispatcher.jl
+++ src/dispatcher.jl
@@ -1,5 +1,3 @@
-using LIBLINEAR
-
 function random_dispatch(source_dir, k, num_learners, dim)
     files = files_in_dir(source_dir)
     np = nprocs()
@@ -25,11 +23,12 @@ function random_dispatch(source_dir, k, num_learners, dim)
     end
     @printf("Dispatched examples %d in %.2f sec (%.2f examples / sec)\n",
             dispatched, duration, dispatched / duration)
-    for p in 2:num_learners+1
-        remotecall_wait(p, list_data)
-    end
-    for p in 2:num_learners+1
-        remotecall_wait(p, train_models, dim)
+    @sync begin
+        for p in 2:num_learners+1
+            @async begin
+                remotecall_wait(p, train_models, dim)
+            end
+        end
     end
 end
 
@@ -55,30 +54,4 @@ function random_dispatch_worker(file, k, num_learners, dim)
         end
     end
     return count
-end
-
-const learner_data = Dict{Int, Tuple{Vector{Int}, Vector{Float64}}}()
-const models = Dict{Int, Model}
-
-function learner_append_data(cix, ys, xs)
-    global learner_data
-    if !haskey(learner_data, cix)
-        learner_data[cix] = (Int[], Float64[])
-    end
-    append!(learner_data[cix][1], ys)
-    append!(learner_data[cix][2], xs)
-end
-
-function list_data()
-    for (k,v) in learner_data
-        println("$k : $(length(v[1])) examples.")
-    end
-end
-
-function train_models(dim)
-    for (k, v) in learner_data
-        ys, xs_unshaped = v
-        xs = reshape(xs_unshaped, dim, div(length(xs_unshaped), div))
-        models[k] = linear_train(ys, xs, verbose=true)
-    end
 end
